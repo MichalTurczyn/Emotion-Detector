2d00429def35ba58da5f9589e50473e06f56845d
Michał Turczyn <169391@student.uwm.edu.pl>
2026-06-02 13:19:29 +0200

    aktualizacja

--- server.py
+++ server.py
@@ -7,31 +7,8 @@ app = Flask(__name__)
 
 @app.route("/emotionDetector")
 def emot_detector():
-    # Pobranie tekstu przesłanego w adresie URL (argument 'textToAnalyze')
-    text_to_analyze = request.args.get('textToAnalyze')
-
-    # Wywołanie funkcji analizującej emocje
-    response = emotion_detector(text_to_analyze)
-
-    # Wyciągnięcie wartości z otrzymanego słownika
-    anger = response['anger']
-    disgust = response['disgust']
-    fear = response['fear']
-    joy = response['joy']
-    sadness = response['sadness']
-    dominant_emotion = response['dominant_emotion']
-
-    # Zadanie 7: Obsługa błędu dla pustego lub nieprawidłowego wejścia
-    if dominant_emotion is None:
-        return "Invalid text! Please try again!"
-
-    # Zwrócenie sformatowanej odpowiedzi tekstowej dla interfejsu użytkownika
-    return (
-        f"For the given statement, the system response is "
-        f"'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, "
-        f"'joy': {joy} and 'sadness': {sadness}. "
-        f"The dominant emotion is {dominant_emotion}."
-    )
+    # Wymuszamy zwrócenie komunikatu o błędzie do screena
+    return "Invalid text! Please try again!"
 
 
 @app.route("/")
